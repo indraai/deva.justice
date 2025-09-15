eda8e6855b4802b799ff3bbdc47c94cce146d93f
Quinn Michaels <quinn@indra.ai>
2025-09-15 09:34:56 -0700

    Update Justice Deva to VLA:71153924143258087294

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const info = {
   bugs: pkg.bugs.url,
   author: pkg.author,
   license: pkg.license,
+  VLA: pkg.VLA,
   copyright: pkg.copyright,
 };
 
@@ -41,11 +42,18 @@ const JUSTICE = new Deva({
     }    
   },
   modules: {},
-  deva: {},
   func: {},
   methods: {},
+  onInit(data, resolve) {
+    const {personal} = this.license(); // get the license config
+    const agent_license = this.info().VLA; // get agent license
+    const license_check = this.license_check(personal, agent_license); // check license
+    // return this.start if license_check passes otherwise stop.
+    return license_check ? this.start(data, resolve) : this.stop(data, resolve);
+  }, 
   onReady(data, resolve) {
-    this.prompt(this.vars.messages.ready);
+    const {VLA} = this.info();
+    this.prompt(`${this.vars.messages.ready} > VLA:${VLA.uid}`);
     return resolve(data);
   },
   onError(err, data, reject) {
